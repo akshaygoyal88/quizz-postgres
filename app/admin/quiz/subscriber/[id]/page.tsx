d37e02d96baeffb86b85720972b0cdc6175328f3
“Saurabh <akshay.goyal1008@gmail.com>
2024-03-01 16:32:14 +0530

    fixes

--- app/admin/quiz/subscriber/[id]/page.tsx
+++ app/admin/quiz/subscriber/[id]/page.tsx
@@ -1,16 +1,10 @@
 import LeftSideBar from "@/components/Layout/LeftSidebar";
-import Subscribers from "@/components/QuizApp/AdminPanel/Subscribers";
-import pathName from "@/constants";
+import SubscribersList from "@/components/QuizApp/AdminPanel/SubscribersList";
 
 export default function page({ params }: { params: string }) {
   return (
     <LeftSideBar>
-      <Subscribers
-        url={`${pathName.subscriptionApiRoute.path}/${params.id}`}
-        // quizId={params.id}
-        heading={`Subscribers`}
-        placeholder="search candidate with name"
-      />
+      <SubscribersList quizId={params.id} />
     </LeftSideBar>
   );
 }
--- components/QuizApp/AdminPanel/SubscribersList.tsx
+++ components/QuizApp/AdminPanel/SubscribersList.tsx
@@ -4,36 +4,65 @@ import pathName from "@/constants";
 import { useFetch } from "@/hooks/useFetch";
 import React, { useState, useEffect } from "react";
 import { format } from "date-fns";
-import SelectWrapper from "@/components/Shared/SelectWrapper";
 
-const SubscribersList = ({
-  list,
-  selectedList,
-  selectAll,
-  searchTerm,
-  filteredList,
-  handleSelectAll,
-  handleSubscriberSelection,
-}: {
-  list: [];
-  selectedList: [];
-  selectAll: boolean;
-  searchTerm: string;
-  filteredList: [];
-  handleSelectAll: () => void;
-  handleSubscriberSelection: () => void;
-}) => {
+const SubscribersList = ({ quizId }: { quizId: string }) => {
+  const [selectedSubscribers, setSelectedSubscribers] = useState<any[]>([]);
+  const [selectAll, setSelectAll] = useState(false);
   const [quizName, setQuizName] = useState("");
+
+  const {
+    data: listOfSubscribers,
+    error: listOfSubscribersError,
+    isLoading: listOfSubscribersLoading,
+  } = useFetch({
+    url: `${pathName.subscriptionApiRoute.path}/${quizId}`,
+  });
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     setQuizName(urlParams.get("quizName") || "Quiz Name");
   }, []);
 
-  const listOfSubscribers = searchTerm ? filteredList : list;
+  useEffect(() => {
+    if (
+      !listOfSubscribers?.error &&
+      selectedSubscribers.length === listOfSubscribers?.length
+    ) {
+      setSelectAll(true);
+    } else {
+      setSelectAll(false);
+    }
+  }, [selectedSubscribers]);
 
-  return list ? (
-    <div>
+  const handleSelectAll = () => {
+    if (selectAll) {
+      setSelectedSubscribers([]);
+    } else {
+      setSelectedSubscribers(listOfSubscribers);
+    }
+    setSelectAll(!selectAll);
+  };
+
+  const handleSubscriberSelection = (subscriberId: string) => {
+    const index = selectedSubscribers.findIndex(
+      (subscriber) => subscriber.id === subscriberId
+    );
+    if (index === -1) {
+      setSelectedSubscribers([
+        ...selectedSubscribers,
+        listOfSubscribers.find((subscriber) => subscriber.id === subscriberId),
+      ]);
+    } else {
+      setSelectedSubscribers([
+        ...selectedSubscribers.slice(0, index),
+        ...selectedSubscribers.slice(index + 1),
+      ]);
+    }
+  };
+
+  return listOfSubscribers ? (
+    <div className="sm:px-6">
+      <h2 className="text-xl font-bold mb-4">Subscriber for {quizName}</h2>
       <table className="w-full bg-white shadow-md rounded-lg overflow-hidden">
         <thead className="text-white bg-gray-800">
           <tr>
@@ -50,11 +79,12 @@ const SubscribersList = ({
           </tr>
         </thead>
         <tbody>
-          {listOfSubscribers?.map((subscriber: any) => (
+          {listOfSubscribers.map((subscriber: any) => (
             <tr
               key={subscriber.id}
               className={`${
-                selectedList.findIndex((s) => s.id === subscriber.id) !== -1
+                selectedSubscribers.findIndex((s) => s.id === subscriber.id) !==
+                -1
                   ? "bg-gray-200"
                   : ""
               }`}
@@ -63,7 +93,9 @@ const SubscribersList = ({
                 <input
                   type="checkbox"
                   checked={
-                    selectedList.findIndex((s) => s.id === subscriber.id) !== -1
+                    selectedSubscribers.findIndex(
+                      (s) => s.id === subscriber.id
+                    ) !== -1
                   }
                   onChange={() => handleSubscriberSelection(subscriber.id)}
                 />
@@ -75,7 +107,7 @@ const SubscribersList = ({
               <td className="py-3 px-4">
                 <button
                   className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  // onClick={() => approveSubscriber(subscriber.id)}
+                  //   onClick={() => approveSubscriber(subscriber.id)}
                 >
                   Approve
                 </button>
@@ -88,6 +120,4 @@ const SubscribersList = ({
   ) : null;
 };
 
-const Subscribers = SelectWrapper(SubscribersList);
-
-export default Subscribers;
+export default SubscribersList;
